bc91cd3f020c3ec3a0598052383c06d36d2c1641
ronilpatil <ronilpatil2000@gmail.com>
2024-09-18 21:58:06 +0530

    write unit test to test s3 bucket and dataset

--- tests/unit_test/test_unit.py
+++ tests/unit_test/test_unit.py
@@ -1,4 +1,30 @@
+import boto3
 import pytest
 
-def test_sample1():
-    assert 1 == 1
+
+# function to check bucket exist or not
+def s3Bucket_isEmpty(bucket_name):
+    s3 = boto3.client("s3")
+    response = s3.list_objects_v2(Bucket=bucket_name)
+    return "Contents" not in response
+
+
+# test case to check bucket exist or not
+@pytest.mark.filterwarnings("ignore::DeprecationWarning")
+@pytest.mark.parametrize("bucket", [("delivery-time-estimate-data")])
+def test_s3_bucket_exist(bucket):
+    assert not s3Bucket_isEmpty(bucket), f"Bucket {bucket} not exist"
+
+
+# function to check data exist in bucket or not
+def s3_file_exist(bucket, file):
+    s3 = boto3.client("s3")
+    response = s3.list_objects_v2(Bucket=bucket)
+    return file in [i["Key"] for i in response["Contents"]]
+
+
+# test case to check data exist in bucket or not
+@pytest.mark.filterwarnings("ignore::DeprecationWarning")
+@pytest.mark.parametrize("bucket, file", [("delivery-time-estimate-data", "train.csv")])
+def test_s3_bucket_has_data(bucket, file):
+    assert s3_file_exist(bucket, file), f"Data {file} not exist"
